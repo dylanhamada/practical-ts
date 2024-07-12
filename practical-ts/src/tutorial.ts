dfc7d19f655ebe4082a795962d931e28698e0e4c
Dylan Hamada <dylan.hamada@gmail.com>
2024-07-11 23:47:15 -0400

    Write Array Examples
    - TypeScript with undefined type
    - TypeScript and arrays

--- practical-ts/src/tutorial.ts
+++ practical-ts/src/tutorial.ts
@@ -42,3 +42,40 @@ requestStatus = 'pending';
 let notSure: any = 4;
 notSure = false;
 notSure = 'maybe a string';
+
+const books = ['1984', 'Brave New World', 'Fahrenheit 451'];
+
+let foundBook: string | undefined;
+
+for (let book of books) {
+  if (book === '1983') {
+    foundBook = book;
+    foundBook = foundBook.toUpperCase();
+    break;
+  }
+}
+
+console.log(foundBook?.length);
+
+// Create a variable orderStatus of type 'processing' | 'shipped' | 'delivered' and assign it the value 'processing'. Then, try to assign it the values 'shipped' and 'delivered'.
+
+let orderStatus: 'processing' | 'shipped' | 'delivered';
+orderStatus = 'processing';
+orderStatus = 'shipped';
+orderStatus = 'delivered';
+
+// Create a variable discount of type number | string and assign it the value 20. Then, try to assign it the value '20%'.
+
+let discount: number | string = 20;
+discount = 20;
+discount = '20%';
+
+let prices: number[] = [100, 75, 42];
+
+let fruit: string[] = ['apple', 'orange'];
+
+let temperatures: number[] = [20, 25, 30]
+
+let colors: string[] ['red', 'green', 'blue']
+
+let mixedArray: (number | string)[] = [1, two, 3]
