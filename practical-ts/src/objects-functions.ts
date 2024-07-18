387e3f8b47e97769b28a4489f632282f374c413e
Dylan Hamada <dylan.hamada@gmail.com>
2024-07-18 00:28:07 -0400

    Add Example That Uses Unions and Objects with Defaults as Parameters

--- practical-ts/src/objects-functions.ts
+++ practical-ts/src/objects-functions.ts
@@ -124,3 +124,67 @@ function processInput(param: string | number): void {
 
 processInput(12);
 processInput('omg');
+
+// using objects in parameters, using destructuring
+function createEmployee({ id }: { id: number }): {
+  id: number;
+  isActive: boolean;
+} {
+  return { id, isActive: id % 2 === 0 };
+}
+
+const first = createEmployee({ id: 1 });
+const second = createEmployee({ id: 2 });
+console.log(first, second);
+
+// alternative way of using an object as a parameter
+function createStudent(student: { id: number; name: string }): void {
+  console.log(`Welcome to the course ${student.name.toUpperCase()}!!!`);
+}
+
+const newStudent = {
+  id: 5,
+  name: 'anna',
+};
+
+createStudent(newStudent);
+
+// if an object is passed inline and has extra parameters not defined in the function declaration, TypeScript will throw an error
+// createStudent({id: 1, name: 'bob', age: 3})
+
+// if an object reference is passed as a parameter, it throws no such error
+const oldStudent = {
+  id: 1,
+  name: 'cob',
+  age: 200,
+};
+
+createStudent(oldStudent);
+
+// Your task is to create a function named processData that accepts two parameters:
+
+// The first parameter, input, should be a union type that can be either a string or a number.
+// The second parameter, config, should be an object with a reverse property of type boolean, by default reverse should be false
+
+// The function should behave as follows:
+
+// If input is of type number, the function should return the square of the number.
+// If input is of type string, the function should return the string in uppercase.
+// If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
+
+function processData(
+  input: string | number,
+  config: { reverse: boolean } = { reverse: false }
+): string | number {
+  if (typeof input === 'number') {
+    return input * input;
+  } else {
+    return config.reverse
+      ? input.toUpperCase().split('').reverse().join('')
+      : input.toUpperCase();
+  }
+}
+
+console.log(processData(10));
+console.log(processData('Hello'));
+console.log(processData('Hello', { reverse: true }));
