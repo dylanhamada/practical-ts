a220e01b1917a35de976c1b25329d390ffd96d44
Dylan Hamada <dylan.hamada@gmail.com>
2024-10-02 13:20:12 -0400

    Add printLength Example to Type Guards

--- practical-ts/src/type-guards.ts
+++ practical-ts/src/type-guards.ts
@@ -24,4 +24,61 @@ function checkValue(val: ValueType): void {
   }
 }
 
-checkValue(value);
+// checkValue(value);
+
+type Dog = {
+  type: 'dog';
+  name: string;
+  bark: () => void;
+};
+
+type Cat = {
+  type: 'cat';
+  name: string;
+  meow: () => void;
+};
+
+type Animal = Dog | Cat;
+
+// Define a function named makeSound that takes one parameter animal of type Animal.
+// Inside the function, use an if statement to check if animal.type is 'dog'.
+// If animal.type is 'dog', TypeScript knows that animal is a Dog in this block. In this case, call the bark method of animal.
+// If animal.type is not 'dog', TypeScript knows that animal is a Cat in the else block. In this case, call the meow method of animal.
+// Now you can call the makeSound function with an Animal as the argument. The function will call the appropriate method (bark or meow) depending on the type of the animal.
+
+function makeSound(animal: Animal): void {
+  if (animal.type === 'dog') {
+    animal.bark();
+  } else {
+    animal.meow();
+  }
+}
+
+const Dalmatian: Dog = {
+  type: 'dog',
+  name: 'Spot',
+  bark() {
+    console.log('Woof!');
+  },
+};
+
+// makeSound(Dalmatian);
+
+// Define a function named printLength that takes one parameter str which can be of type string, null or undefined
+// Inside the function, use an if statement to check if str is truthy. In JavaScript and TypeScript, a truthy value is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy (ie. except for false, 0, -0, 0n, "", null, undefined, and NaN).
+// If str is truthy, it means it's a string (since null and undefined are falsy). In this case, log the length of str using the length property of the string.
+// If str is not truthy (ie. it's either null or undefined), log the string 'No string provided'.
+// Now you can call the printLength function with a string, null or undefined as the argument. The function will print the length of the string if a string is provided, or 'No string provided' otherwise.
+
+function printLength(str: string | null | undefined): void {
+  if (str) {
+    console.log(str.length);
+  } else {
+    console.log('No string provided.');
+  }
+}
+
+// printLength('Hello world!');
+// printLength(undefined);
+// printLength('100');
+// printLength(null);
