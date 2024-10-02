523f7566230c464d80a9ae82599a0b1d587f3343
Dylan Hamada <dylan.hamada@gmail.com>
2024-10-02 17:01:38 -0400

    Update Generic Types Lesson

---
+++ practical-ts/src/generics.ts
@@ -0,0 +1,82 @@
+// let array1: string[] = ['Apple', 'Banana', 'Mango'];
+// let array2: number[] = [1, 2, 3];
+// let array3: boolean[] = [true, false, true];
+
+// This is an alternative way of defining a string array in TypeScript
+let array1: Array<string> = ['Apple', 'Banana', 'Mango'];
+
+// function createString(arg: string): string {
+//   return arg;
+// }
+
+// function createNumber(arg: number): number {
+//   return arg;
+// }
+
+// This is a generic function, which allows the function to operate on different types without losing type information
+// This is in contrast to the two functions above, which are tied to a specific type (string and number, respectively)
+// The <T> denotes any type, and "T" can be any placeholder, including "shakeAndBake" if we want
+function genericFunction<T>(arg: T): T {
+  return arg;
+}
+
+// Generic functions can be reused with different types
+const someStringValue = genericFunction<string>('Hello world');
+const someNumValue = genericFunction<number>(100);
+
+// We can also declare a generic interface
+// Note the "T" is used as a placeholder for any type
+interface GenericInterface<T> {
+  value: T;
+  getValue: () => T;
+}
+
+const genericString: GenericInterface<string> = {
+  value: 'Hello World',
+  getValue() {
+    return this.value;
+  },
+};
+
+// The return type here must be Promise<string>, because by default, an async function returns a promise, so we cannot just define the return type as 'string'
+async function someFunc(): Promise<string> {
+  return 'hello world';
+}
+
+const result = someFunc();
+
+// Non-generic function
+function generateStringArray(length: number, value: string): string[] {
+  let result: string[] = [];
+  result = Array(length).fill(value);
+  return result;
+}
+
+// Generic function that can achieve the same results as the above non-generic function
+function createArray<T>(length: number, value: T): Array<T> {
+  let result: T[] = [];
+  result = Array(length).fill(value);
+  return result;
+}
+
+let arrayStrings = createArray<string>(10, 'hello');
+let arrayNumbers = createArray<number>(15, 100);
+
+// console.log(arrayStrings);
+// console.log(arrayNumbers);
+
+// We're not limited to one generic type, we can use "T", "U", and as many as we need
+function pair<T, U>(param1: T, param2: U): [T, U] {
+  return [param1, param2];
+}
+
+let resultPair = pair<number, string>(123, 'hello');
+
+// Using the 'extends' keyword, we can limit the generic types that our function will accept
+// In this case, we limit the type to 'string', but we can also use 'number', or even a union (ie. string | number)
+function processValue<T extends string>(value: T): T {
+  console.log(value);
+  return value;
+}
+
+processValue('hello');
